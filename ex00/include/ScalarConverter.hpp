9df6d71ac8747cc90a8f6ad3b939204bf8accb8b
Moein Rezaei <moeinrezaei330@gmail.com>
2024-02-09 16:01:10 +0100

    the first version of the project is added.

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -1,9 +1,11 @@
 # ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
-#include <iostream>
 #include <sstream>
+#include <iostream>
+#include <iomanip>
 #include <limits>
+#include <cmath>
 
 class ScalarConverter
 {
